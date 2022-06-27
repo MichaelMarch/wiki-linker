db07d529ecc97e73eaa28373d3cc1cd9629bb4e5
MichaelMarch <s222074@student.uek.krakow.pl>
2022-06-27 23:03:47 +0200

    Allow indexing and slicing of OrderedSet

--- ordered_set.py
+++ ordered_set.py
@@ -1,11 +1,13 @@
 # This class relies on a feature of Python 3.7+ which guarantees that dictionaries preserve order on insertion.
 
-# The class provides O(1) look up of elements while keeping the order and sacrificing the memory.
+# The class provides O(1) look up of elements while keeping the order, sacrificing the memory.
 # All items of this collection are unique
+
+
 class OrderedSet:
     def __init__(self):
-        self.set: set = set()
-        self.map: dict = {}
+        self.map = dict()
+        self.set = set()
 
     def __len__(self) -> int:
         return len(self.map.keys())
@@ -20,6 +22,11 @@ class OrderedSet:
         if value not in self.map:
             self.map[value] = None
             self.set.add(value)
-    # TODO: benchmark set.intersecion
+
+    def __getitem__(self, index):
+        if isinstance(index, slice):
+            return OrderedSet(values=list(self.map.keys())[index])
+        return list(self.map.keys())[index]
+
     def count_intersections_with(self, ordered_set: "OrderedSet") -> int:
         return len(set.intersection(self.set, ordered_set.set))
